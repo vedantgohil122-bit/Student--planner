d6d6d669f4dae4e614611e069771005bac5a8b30
Vedant <vedantgohil122@email.com>
2026-05-25 12:42:05 +0530

    Day 2: JavaScript variables, data types and template literals

---
+++ practice/day2.js
@@ -0,0 +1,10 @@
+const studentname = "vedant";
+const age = 21;
+const isEnrolled = true;
+const score = "null";
+
+console.log (`My name is ${studentname}`);
+console.log(`I am ${age} years old`);
+console.log(`Enrolled: ${isEnrolled}`);
+console.log(`Score: ${score}`);
+
